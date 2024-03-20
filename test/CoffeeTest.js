f156d8dd688c2f54ba7d8c7e6e3da9b06b82f905
Khoroshavtsev Nikita <natifick@yandex.ru>
2024-03-20 15:58:30 +0300

    Fixed tests to 'implicit calls' of transfers

--- test/CoffeeTest.js
+++ test/CoffeeTest.js
@@ -62,7 +62,7 @@ describe("Testing Cofee Crowdfunding", function () {
     const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
     
     await fakeCurrencyToken.connect(donor1).approve(coffeeToken, 10);
-    await coffeeToken.deposit(donor1, 10);
+    await coffeeToken.connect(donor1).deposit(10);
 
     // donor1 spent all fake currency
     expect(await fakeCurrencyToken.balanceOf(donor1.address)).to.equal(0);
@@ -70,20 +70,19 @@ describe("Testing Cofee Crowdfunding", function () {
     expect(await coffeeToken.balanceOf(donor1.address)).to.equal(10);
     
     expect(await coffeeToken.balance()).to.equal(10);
-    expect(coffeeToken.withdraw(donor1, 1)).to.be.reverted;
+    expect(coffeeToken.connect(donor1).withdraw(1)).to.be.reverted;
   })
 
   it("Test 2: complete deposit", async function() {
     const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
   
     await fakeCurrencyToken.connect(donor1).approve(coffeeToken, 10);
-    await coffeeToken.deposit(donor1, 10);
+    await coffeeToken.connect(donor1).deposit(10);
     await fakeCurrencyToken.connect(donor2).approve(coffeeToken, 20);
-    await coffeeToken.deposit(donor2, 20);
+    await coffeeToken.connect(donor2).deposit(20);
 
     // check that 5 tokens returned to donor2
     expect(await fakeCurrencyToken.balanceOf(donor2.address)).to.equal(5);
-    expect(await coffeeToken.balance()).to.equal(25);
     expect(await coffeeToken.balance()).to.equal(25);
   })
 
@@ -91,8 +90,8 @@ describe("Testing Cofee Crowdfunding", function () {
     const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
 
     await fakeCurrencyToken.connect(donor1).approve(coffeeToken, 10);
-    await coffeeToken.deposit(donor1, 10);
+    await coffeeToken.connect(donor1).deposit(10);
     await fakeCurrencyToken.connect(donor2).approve(coffeeToken, 15);
-    await coffeeToken.deposit(donor2, 15);
+    await coffeeToken.connect(donor2).deposit(15);
   })
 })

3e5c1ff58a25c34313482bba5506fbd0c7959462
Khoroshavtsev Nikita <natifick@yandex.ru>
2024-03-20 15:38:25 +0300

    Try living without factory. Pass first two tests

--- test/CoffeeTest.js
+++ test/CoffeeTest.js
@@ -20,8 +20,6 @@ describe("Testing Cofee Crowdfunding", function () {
     await fakeCurrencyToken.connect(fakeCurrencyBank).mint(donor1.address, 10);
     await fakeCurrencyToken.connect(fakeCurrencyBank).mint(donor2.address, 20);
 
-    await fakeCurrencyToken.connect(coffeeShop);
-
     // 2. deploy crowdfunding contract for coffeShop with goal in this currency
     // (who owns this is a mystery no one should know)
     // const CoffeeFactory = await ethers.getContractFactory("CoffeeFactory");
@@ -38,8 +36,7 @@ describe("Testing Cofee Crowdfunding", function () {
     // console.log("swrrefs", coffeeToken);
 
     const CoffeeToken = await ethers.getContractFactory("CoffeeToken");
-    const coffeeToken = await CoffeeToken.deploy("CoffeeToken", "CFT", 25, coffeeShop.address);
-    // console.log(coffeeToken);
+    const coffeeToken = await CoffeeToken.deploy("CoffeeToken", "CFT", 25, fakeCurrencyToken);
 
     // A bit strange way for deployment of the token
     // const CoffeeToken = await ethers.getContractFactory("MyCoffeeToken");
@@ -57,36 +54,39 @@ describe("Testing Cofee Crowdfunding", function () {
     expect(await fakeCurrencyToken.balanceOf(coffeeShop.address)).to.equal(0)
 
     expect(await coffeeToken.balance()).to.equal(25);
+    expect(await coffeeToken.balanceOf(donor1.address)).to.equal(0);
+    expect(await coffeeToken.balanceOf(donor2.address)).to.equal(0);
   });
 
   it("Test 1: partial deposit", async function() {
     const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
-    await coffeeToken.deposit(donor1.address, 10);
+    await fakeCurrencyToken.connect(donor1).approve(coffeeToken, 10);
+    await coffeeToken.connect(donor1).deposit(10);
 
     // donor1 spent all fake currency
     expect(await fakeCurrencyToken.balanceOf(donor1.address)).to.equal(0);
     // and recieved coffeTokens in exchange
     expect(await coffeeToken.balanceOf(donor1.address)).to.equal(10);
     
-    expect(await coffeToken.balance()).to.equal(10);
-    expect(await coffeToken.withdraw(1)).to.be.reverted;
+    expect(await coffeeToken.balance()).to.equal(15);
+    // expect(await coffeeToken.withdraw(1)).to.be.reverted;
   })
 
   it("Test 2: complete deposit", async function() {
-    const {coffeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
+    const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
   
-    await coffeToken.connect(donor1.address).deposit(10);
-    await coffeToken.connect(donor2.address).deposit(20);
+    await coffeeToken.connect(donor1).deposit(10);
+    await coffeeToken.connect(donor2).deposit(20);
 
     // check that 5 tokens returned to donor2
     expect(await fakeCurrencyToken.balanceOf(donor2.address)).to.equal(5);
-    expect(await coffeToken.balance()).to.equal(25);
+    expect(await coffeeToken.balance()).to.equal(25);
   })
 
   it ("Test 3: withdraw coffee tokens for coffee", async function() {
-    const {coffeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
+    const {coffeeToken, fakeCurrencyToken, coffeeShop, donor1, donor2} = await deployFixture();
 
-    await coffeToken.connect(donor1).deposit(10);
-    await coffeToken.connect(donor2).deposit(15); 
+    await coffeeToken.connect(donor1).deposit(10);
+    await coffeeToken.connect(donor2).deposit(15); 
   })
 })
